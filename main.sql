7319286a05c5914a2552a1c11f1f240a5cbc6663
ruandev07 <162300757+ruandev07@users.noreply.github.com>
2025-01-06 19:28:44 -0300

    Second Commit

--- main.sql
+++ main.sql
@@ -7,8 +7,19 @@ CREATE DATABASE ebac;
 -- Criando o Esquema
 CREATE SCHEMA store;
 
--- Criando tabela no nosso esquema da seguinte forma
--- Sempre colocar o esquema em aspas Duplas e o nome do esquema ("store") seguido de um (.) e o nome da tabela que queira criar (exemplo abaixo) e alimentando a tabela
-CREATE TABLE "store".customes (
-    
+-- Criando tabela no nosso esquema da seguinte forma.
+-- Sempre colocar o esquema em aspas Duplas e o nome do esquema ("store") seguido de um (.) e o nome da tabela que queira criar (exemplo abaixo) e alimentando a tabela.
+CREATE TABLE "store".customer (
+    customer_id SERIAL PRIMARY KEY, -- SERIAL PRIMARY KEY, nada mais e que uma forma de deixar nosso ID automático (Exemplo (Products_id)) ele ja criaria um ID automático para nos.
+    name VARCHAR(255) NOT NULL, -- o (NOT NULL), nada mais é que uma forma de não deixar vazio o nome na tabela 
+    email VARCHAR(255) UNIQUE NOT NULL, -- (UNIQUE), significa que na tabela de email quando passado, ele sera unico em toda a tabela e o (NOT NULL) não deixara vazio o nome na tabela.
+    phone VARCHAR(20),
+    document_number VARCHAR(20) UNIQUE,
+    address VARCHAR(255),
+    city VARCHAR(100),
+
+    )
+
+CREATE TABLE "store".Products (
+
 )
