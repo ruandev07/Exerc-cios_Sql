ead5dddd8f2123c2be85ed4cc6e1b74330f9df16
Ruan Nunes <ruanbdcnf@gmail.comm>
2025-01-11 16:11:58 -0300

    Third Commit

--- main.sql
+++ main.sql
@@ -16,10 +16,22 @@ CREATE TABLE "store".customer (
     phone VARCHAR(20),
     document_number VARCHAR(20) UNIQUE,
     address VARCHAR(255),
-    city VARCHAR(100),
+    city VARCHAR(100)
+    );
 
-    )
-
+--- Criando tabela de Produtos
 CREATE TABLE "store".Products (
+    Products_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(100),
+    stock_quantity INTEGER NOT NULL,
+    price  INT(50) NOT NULL
+);
 
-)
+--- Criando tabela de Estoque
+CREATE TABLE "store".Products_stock (
+    stock_id SERIAL PRIMARY KEY,
+    Product_id INT(11) NULL DEFAULT NULL,
+    quantity INTEGER NOT NULL CHECK (quantity >=0), --Garantir que a quantidade  não seja negativa
+    unit_value DECIMAL(9,3) NULL DEFAULT "0.00"
+);
